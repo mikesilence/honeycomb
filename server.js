3f0b397b142b6327252828d709966929dd96e3db
Mike Khromov <m.hromov@afisha.ru>
2016-09-06 14:08:01 +0300

    complite this.issues

--- server.js
+++ server.js
@@ -3,12 +3,33 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
+
+app.use(express.static('static'));
+app.use('/storage', express.static('/storage.json'));
+
+app.post('/storage', function(req, res) {
+    var body = '';
+    var filePath = path.join(__dirname + '/storage.json');
+
+    req.on('data', function(data) {
+      body += data;
+    });
+
+    req.on('end', function (){
+      fs.writeFile(filePath, body, function() {
+        res.end();
+      });
+    });
+});
 
 app.get('/',function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.use(express.static('static'));
+app.get('/storage',function(req, res){
+  res.sendFile(path.normalize(__dirname + '/storage.json'))
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
